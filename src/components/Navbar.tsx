8ed09d7c29e235a788c4c387b95b45a00e6853ef
ipunganwar <ipung.anwar1@gmail.com>
2025-03-19 16:11:29 +0700

    core: update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 import Button from "./Button";
 import gsap from "gsap";
