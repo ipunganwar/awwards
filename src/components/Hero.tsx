b75903fdf756524e0176e779fb875dada0de09b8
ipunganwar <ipung.anwar1@gmail.com>
2025-03-19 16:30:49 +0700

    core: update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
